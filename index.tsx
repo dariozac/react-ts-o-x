6adf0c8beb764e69efdbe6d41f04ad8c3cf8a8f2
Dario Zaccagnini <dario.zaccagnini@gmail.com>
2021-10-05 06:44:57 +0400

    initial commit

--- index.tsx
+++ index.tsx
@@ -1,13 +1,10 @@
-import * as React from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
-
-interface AppProps {}
+import * as React from "react";
+import { render } from "react-dom";
+import "./style.css";
 
 enum Marker {
-  x = 'X',
-  o = 'O',
+  x = "X",
+  o = "O"
 }
 
 const Square123 = ({
@@ -15,34 +12,55 @@ const Square123 = ({
   text,
   plays,
   play,
-  currentPlayer,
+  currentPlayer
 }: {
   key: number;
   text: number;
   marker: Marker | undefined;
   currentPlayer: Marker;
-  plays: any;
-  play: any;
+  plays: Map<number, Marker>;
+  play: (event, number, Marker) => void;
 }) => (
   <div
     key={key}
     onClick={(e) => play(e, text as number, currentPlayer)}
-    className={'square'}
+    className={"square"}
   >
-    {plays.has(text) ? plays.get(text) : 'open'}
+    {plays.has(text) ? plays.get(text) : "open"}
   </div>
 );
 
+const winningLines: number[][] = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [2, 5, 7]
+];
+
 const App = () => {
+  const [winner, setWinner] = React.useState<Marker | undefined>(undefined);
   const [playsState, setPlaysState] = React.useState<Map<number, Marker>>(
-    new Map<number, Marker>([[1, Marker.x]])
+    new Map<number, Marker>([])
   );
+
+  const checkForWin = () => {
+    return winningLines.some((line) =>
+      line.every((x) => playsState.get(x) === currentPlayer)
+    );
+  };
 
   const play = (event: React.MouseEvent<HTMLDivElement>, text: number) => {
     event.preventDefault();
     console.log(event.currentTarget, text);
     setPlaysState(playsState.set(text, currentPlayer));
     console.dir(Array.from(playsState));
+    if (checkForWin()) {
+      setWinner(currentPlayer);
+    }
     setCurrentPlayer(currentPlayer === Marker.x ? Marker.o : Marker.x);
   };
   const getGrid = () => {
@@ -61,13 +79,29 @@ const App = () => {
   };
   const [currentPlayer, setCurrentPlayer] = React.useState<Marker>(Marker.o);
 
+  if (winner) {
+    return (
+      <h1>
+        The winner is: {winner}{" "}
+        <button
+          onClick={() => {
+            setPlaysState(new Map());
+            setWinner(undefined);
+          }}
+        >
+          Play Again
+        </button>
+      </h1>
+    );
+  }
+
   return (
     <div>
       <h1>Current Player: {currentPlayer}</h1>
-      <div className={'grid'}>{getGrid()}</div>
+      <div className={"grid"}>{getGrid()}</div>
       <button onClick={() => setPlaysState(new Map())}>Start Again?</button>
     </div>
   );
 };
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
